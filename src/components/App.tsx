ddc3652fbbd8587bdd409b8b32c38e38bd9c95e6
r19 <r19>
2021-03-10 10:21:15 +0300

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import EmployeeAdd from "./EmployeeAddForm";
+export default function App() {
+  return (
+    <div className="container">
+      <EmployeeAdd />
+    </div>
+  );
+}
