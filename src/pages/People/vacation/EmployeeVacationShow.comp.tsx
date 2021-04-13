613b55fdbf6325b3063a4d860c41b6d02a7ccb29
Ahmed Hafez <ahmedmhiz2010@gmail.com>
2021-04-13 14:55:47 +0300

    working on add vacation / transfer / leave comp

--- src/pages/People/vacation/EmployeeVacationShow.comp.tsx
+++ src/pages/People/vacation/EmployeeVacationShow.comp.tsx
@@ -1,9 +1,43 @@
 import React, { ChangeEvent, useState } from "react";
 import { useSelector } from "react-redux";
+import OptionInputField from "../../../components/FormElements/OptionInputField";
+import Tabs from "../../../components/Tabs/Tab.comp";
 import { selectFilteredEmployees } from "../../../store";
-interface EmployeeVacationShowProps {}
+import { leaveOptions } from "../../../static/StaticOptions";
+interface Props {}
 
-const EmployeeVacationShow: React.FC<EmployeeVacationShowProps> = (props) => {
+const Vacation = () => {
+  return (
+    <form>
+      <div className="control">
+        <label htmlFor="from">From</label>
+        <input id="from" type="date" />
+      </div>
+
+      <div className="control">
+        <label htmlFor="to">To</label>
+        <input id="to" type="date" />
+      </div>
+
+      <div className="control">
+        <label htmlFor="duration">duration</label>
+        <input type="text" name="duration" id="duration" />
+      </div>
+
+      <input type="submit" value="submit" />
+    </form>
+  );
+};
+
+const Leave = () => {
+  return (
+    <form>
+      <OptionInputField label="Leave" name="leave" options={leaveOptions} />
+    </form>
+  );
+};
+
+const EmployeeVacationShow: React.FC<Props> = (props) => {
   const employees = useSelector(selectFilteredEmployees);
   const selectedCount = employees.length;
 
@@ -12,8 +46,8 @@ const EmployeeVacationShow: React.FC<EmployeeVacationShowProps> = (props) => {
   const [duration, setDuration] = useState("");
 
   const handleDateSelection = (e: ChangeEvent<HTMLInputElement>) => {
-    e.target.value
-  }
+    e.target.value;
+  };
 
   const HandleSelection = (): JSX.Element => {
     if (selectedCount == 0) {
@@ -24,27 +58,14 @@ const EmployeeVacationShow: React.FC<EmployeeVacationShowProps> = (props) => {
     }
     return (
       <div>
-        <b>
-          {JSON.stringify(employees[0])}
-          <form>
-            <div className="control">
-              <label htmlFor="from">From</label>
-              <input id="from" type="date" />
-            </div>
-
-            <div className="control">
-              <label htmlFor="to">To</label>
-              <input id="to" type="date" />
-            </div>
-
-            <div className="control">
-              <label htmlFor="duration">duration</label>
-              <input type="text" name="duration" id="duration" />
-            </div>
-
-            <input type="submit" value="submit" />
-          </form>
-        </b>
+        <b>{JSON.stringify(employees[0])}</b>
+        <Tabs
+          tabs={[
+            { title: "Vacation", child: Vacation },
+            { title: "Leave Permission", child: Leave },
+            { title: "Transfer" },
+          ]}
+        />
       </div>
     );
   };
