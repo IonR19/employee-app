7c3cae9702b71a94c5a354aa1f0529d37e060cea
Ahmed Hafez <ahmedmhiz2010@gmail.com>
2021-04-14 13:48:19 +0300

    add calculation for vacation time

--- src/pages/People/vacation/EmployeeVacationShow.comp.tsx
+++ src/pages/People/vacation/EmployeeVacationShow.comp.tsx
@@ -4,24 +4,72 @@ import OptionInputField from "../../../components/FormElements/OptionInputField"
 import Tabs from "../../../components/Tabs/Tab.comp";
 import { selectFilteredEmployees } from "../../../store";
 import { leaveOptions } from "../../../static/StaticOptions";
+import { DateTime } from "luxon";
+
 interface Props {}
 
 const Vacation = () => {
+  // window.luxon = DateTime;
+  const [state, setState] = useState({
+    from: "",
+    to: "",
+    duration: "",
+  });
+
+  const handleDateSelection = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+  React.useEffect(() => {
+    const { from, to } = state;
+    if (from && to) {
+      console.log(from, to);
+      const st = DateTime.fromISO(to);
+      const en = DateTime.fromISO(from);
+      const diff = st.diff(en, ["days"]);
+      setState({ ...state, duration: "" + diff.days });
+    }
+  }, [state.to]);
+
+  React.useEffect(() => {
+    const { from, duration } = state;
+    if (from && duration) {
+      const en = DateTime.fromISO(from).plus({
+        days: +duration,
+      });
+      console.log(en.toISODate());
+      setState({
+        ...state,
+        to: en.toISODate(),
+      });
+    }
+  }, [state.duration]);
+
+  const { from, to, duration } = state;
   return (
-    <form>
+    <form onSubmit={(e) => e.preventDefault()}>
       <div className="control">
         <label htmlFor="from">From</label>
-        <input id="from" type="date" />
+        <input id="from" name="from" type="date" value={from} onChange={handleDateSelection} />
       </div>
 
       <div className="control">
         <label htmlFor="to">To</label>
-        <input id="to" type="date" />
+        <input id="to" name="to" type="date" value={to} onChange={handleDateSelection} />
       </div>
 
       <div className="control">
         <label htmlFor="duration">duration</label>
-        <input type="text" name="duration" id="duration" />
+        <input
+          type="text"
+          name="duration"
+          id="duration"
+          value={duration}
+          onChange={handleDateSelection}
+        />
       </div>
 
       <input type="submit" value="submit" />
@@ -40,14 +88,6 @@ const Leave = () => {
 const EmployeeVacationShow: React.FC<Props> = (props) => {
   const employees = useSelector(selectFilteredEmployees);
   const selectedCount = employees.length;
-
-  const [from, setFrom] = useState("");
-  const [to, setTo] = useState("");
-  const [duration, setDuration] = useState("");
-
-  const handleDateSelection = (e: ChangeEvent<HTMLInputElement>) => {
-    e.target.value;
-  };
 
   const HandleSelection = (): JSX.Element => {
     if (selectedCount == 0) {
