a1af20e88f2fedb4e231ac9cd6633b93db8a84b7
Ahmed Hafez <ahmedmhiz2010@gmail.com>
2021-04-26 14:38:36 +0300

    init react i18n

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
 import { Provider } from "react-redux";
+import "./localization/i18n";
 
 import "./style.scss";
 import { store } from "./store/store";
