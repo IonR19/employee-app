72ce57f6712065ccf9da4dd288e005327737dec2
ionR19 <ahmedmhiz2010@gmail.com>
2021-05-08 00:00:01 +0300

    resetfilter when leaving page

--- src/pages/People/vacation/EmployeeVacation.page.tsx
+++ src/pages/People/vacation/EmployeeVacation.page.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { resetFilter } from "../../../store";
 import EmployeeVacationFilter from "./EmployeeVacationFilter.comp";
 import EmployeeVacationShow from "./EmployeeVacationShow.comp";
 
 interface EmployeeVacationProps {}
 
 const EmployeeVacation: React.FC = (props: EmployeeVacationProps) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    return () => {
+      dispatch(resetFilter());
+    };
+  }, []);
   return (
     <div>
       <EmployeeVacationFilter />
