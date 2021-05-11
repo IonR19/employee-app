e1ef925c88e096e82adfe484e043aad4a174f7f3
Ahmed Hafez <ahmedmhiz2010@gmail.com>
2021-05-11 13:06:11 +0300

    add logger middleware and move to configureStore

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,11 @@
-import { getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import employeeSlice from "./employees/employee.slice";
 import OrdersReducer from "./orders/orders.reducers";
 import SettingsReducer from "./settings/settings.reducers";
 import VacationReducer from "./vacations/vacation.reducers";
-
+import logger from "redux-logger";
 const reducers = combineReducers({
   employees: employeeSlice.reducer,
   vacations: VacationReducer,
@@ -13,9 +13,13 @@ const reducers = combineReducers({
   settings: SettingsReducer,
 });
 
-const middlewares = [...getDefaultMiddleware()];
+const middlewares = [...getDefaultMiddleware(), logger];
 
-export const store = createStore(reducers, applyMiddleware(...middlewares));
+// export const store = createStore(reducers, applyMiddleware(...middlewares));
+export const store = configureStore({
+  reducer: reducers,
+  middleware: middlewares,
+});
 export type RootState = ReturnType<typeof reducers>;
 export type DefaultRootState = RootState;
 export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
