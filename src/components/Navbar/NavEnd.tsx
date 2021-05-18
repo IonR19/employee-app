0e6d20e2b6f84d420f212dc27dba2a604ee056c4
Ahmed Hafez <ahmedmhiz2010@gmail.com>
2021-05-18 09:53:00 +0300

    move section option to navbar

--- src/components/Navbar/NavEnd.tsx
+++ src/components/Navbar/NavEnd.tsx
@@ -1,18 +1,48 @@
 import React from "react";
+import { Dropdown, Icon, Navbar } from "react-bulma-components";
+import { useTranslation } from "react-i18next";
+import { useDispatch } from "react-redux";
+import { SectionOptions } from "../../static/StaticOptions";
+import { setSite, useTypedSelector } from "../../store";
 import NavbarChangeSettings from "./NavbarChangeSettings.comp";
 
 const NavEnd: React.FC = () => {
+  const dispatch = useDispatch();
+  const site = useTypedSelector((s) => s.employees.site);
+  const { t } = useTranslation();
+
   return (
     <div className="navbar-end">
+      <Navbar.Item>
+        <Dropdown
+          icon={
+            <Icon>
+              <i aria-hidden="true" className="fas fa-angle-down" />
+            </Icon>
+          }
+          label={t("work_location")}
+          value={site}
+          onChange={(e: string) => dispatch(setSite(e))}
+        >
+          {SectionOptions.map((option, i) => (
+            <Dropdown.Item renderAs="a" key={i} value={option.value}>
+              {t(option.title ?? "?")}
+            </Dropdown.Item>
+          ))}
+          <Dropdown.Item renderAs="a" value={undefined}>
+            {t("all")}
+          </Dropdown.Item>
+        </Dropdown>
+      </Navbar.Item>
       <NavbarChangeSettings />
-      <div className="navbar-item">
+      {/* <div className="navbar-item">
         <div className="buttons">
           <a className="button is-primary">
             <strong>Sign up</strong>
           </a>
           <a className="button is-light">Log in</a>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
--- src/components/Navbar/NavbarChangeSettings.comp.tsx
+++ src/components/Navbar/NavbarChangeSettings.comp.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-import { Dropdown, Icon, Menu } from "react-bulma-components";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
-import { SectionOptions } from "../../static/StaticOptions";
-import { setSite, useTypedSelector } from "../../store";
 import { ChangeLanguage } from "../../store/settings";
 
 interface Props {}
@@ -20,8 +17,6 @@ const NavbarChangeSettings: React.FC<Props> = (props) => {
     i18n.changeLanguage(newLang);
   };
 
-  const site = useTypedSelector((s) => s.employees.site);
-
   return (
     <div className="navbar-item has-dropdown is-hoverable">
       <a className="navbar-link">{t("settings")}</a>
@@ -30,25 +25,6 @@ const NavbarChangeSettings: React.FC<Props> = (props) => {
         <a className="navbar-item" onClick={languageChangeHandler}>
           {t("switch_lng")}
         </a>
-        <Dropdown
-          icon={
-            <Icon>
-              <i aria-hidden="true" className="fas fa-angle-down" />
-            </Icon>
-          }
-          label={t("work_location")}
-          value={site}
-          onChange={(e: string) => dispatch(setSite(e))}
-        >
-          {SectionOptions.map((option, i) => (
-            <Dropdown.Item renderAs="a" key={i} value={option.value}>
-              {t(option.title ?? "?")}
-            </Dropdown.Item>
-          ))}
-          <Dropdown.Item renderAs="a" value={undefined}>
-            {t("all")}
-          </Dropdown.Item>
-        </Dropdown>
       </div>
     </div>
   );
